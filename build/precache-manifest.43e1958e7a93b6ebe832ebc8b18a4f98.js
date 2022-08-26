fef635b6d280bb98fb85b23c85e296dcb3fd38e7
Nanashi Li <tihan-nico@plexus.dev>
2022-08-26 19:37:04 +0200

    Added build folder

---
+++ build/precache-manifest.43e1958e7a93b6ebe832ebc8b18a4f98.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9019689822988d0e3cce",
+    "url": "/static/css/main.503d531b.chunk.css"
+  },
+  {
+    "revision": "9019689822988d0e3cce",
+    "url": "/static/js/main.90196898.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "b717be2ed633dcc60ff2",
+    "url": "/static/js/2.b717be2e.chunk.js"
+  },
+  {
+    "revision": "d2051469dccdba7ca17d",
+    "url": "/static/js/runtime~main.d2051469.js"
+  },
+  {
+    "revision": "466e42a653474aea45cb2bb87ab2a395",
+    "url": "/static/media/aurora-editor-main.466e42a6.png"
+  },
+  {
+    "revision": "99b46e11f366af56bf63532441d6aebf",
+    "url": "/static/media/small.99b46e11.png"
+  },
+  {
+    "revision": "c2d7d3ae23a184303d15a8b19a29ae91",
+    "url": "/static/media/large_2x.c2d7d3ae.png"
+  },
+  {
+    "revision": "c546f8f033936b2d60577105a4d1bbfb",
+    "url": "/static/media/large.c546f8f0.png"
+  },
+  {
+    "revision": "fc11eadcb317049802e22734d9ef0f8e",
+    "url": "/static/media/medium.fc11eadc.png"
+  },
+  {
+    "revision": "ffcb2379a4dbc416615110282fe4077c",
+    "url": "/index.html"
+  }
+];
