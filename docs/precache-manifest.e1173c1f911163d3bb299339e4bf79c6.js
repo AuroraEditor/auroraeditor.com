3f784e3cc0c74a48c5a89ae968d2552725d054e2
Wesley de Groot <email@wesleydegroot.nl>
2022-08-26 19:50:53 +0200

    Test for building

---
+++ docs/precache-manifest.e1173c1f911163d3bb299339e4bf79c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a06a5d606844f6ba5d2",
+    "url": "/static/css/main.4f618829.chunk.css"
+  },
+  {
+    "revision": "9a06a5d606844f6ba5d2",
+    "url": "/static/js/main.9a06a5d6.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "ca360c0778cff01565a8",
+    "url": "/static/js/2.ca360c07.chunk.js"
+  },
+  {
+    "revision": "d2051469dccdba7ca17d",
+    "url": "/static/js/runtime~main.d2051469.js"
+  },
+  {
+    "revision": "466e42a653474aea45cb2bb87ab2a395",
+    "url": "/static/media/aurora-editor-main.466e42a6.png"
+  },
+  {
+    "revision": "fc11eadcb317049802e22734d9ef0f8e",
+    "url": "/static/media/medium.fc11eadc.png"
+  },
+  {
+    "revision": "c546f8f033936b2d60577105a4d1bbfb",
+    "url": "/static/media/large.c546f8f0.png"
+  },
+  {
+    "revision": "99b46e11f366af56bf63532441d6aebf",
+    "url": "/static/media/small.99b46e11.png"
+  },
+  {
+    "revision": "c2d7d3ae23a184303d15a8b19a29ae91",
+    "url": "/static/media/large_2x.c2d7d3ae.png"
+  },
+  {
+    "revision": "66b7ac739041e388487f7837b42faa42",
+    "url": "/index.html"
+  }
+];
