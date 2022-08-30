263815d4ebd1affed2188daaf5c3991d49d0ed66
Wesley De Groot <email@wesleydegroot.nl>
2022-08-30 16:26:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./Assets/aurora/styles/aurora-global.css"
 import "./Assets/aurora/styles/aurora-footer.css"
 import "./Assets/aurora/styles/aurora-ribbon.css"
+import useScript from './Base/Hooks/useScript';
 import NavHeader from "./Base/Components/NavHeader"
 import MainPage from "./Base/Core/Main"
 import Included from './Base/Core/Included';
@@ -11,6 +12,8 @@ import Footer from "./Base/Components/Footer";
 import Terms from './Base/Core/Terms';
 
 function App() {
+  useScript("./Assets/aurora/scripts/aurora-nav.js")
+  useScript("./Assets/aurora/scripts/color-scheme-toggle.js")
   return (
     <div id="overview" className="dmf" data-color-scheme="light">
       <Ribbon />
@@ -19,7 +22,6 @@ function App() {
       <NavHeader />
       <label id="auroranav-curtain" htmlFor="auroranav-menustate" />
       <div id="auroranav-viewport-emitter" data-viewport-emitter-dispatch="" data-viewport-emitter-state="{&quot;viewport&quot;:&quot;large&quot;,&quot;orientation&quot;:&quot;landscape&quot;,&quot;retina&quot;:false}"></div>
-      <script type="text/javascript" src="./Assets/aurora/scripts/aurora-nav.js"></script>
       <main>
         <Routes>
           <Route exact path="/" element={<MainPage />} />
@@ -28,7 +30,6 @@ function App() {
         </Routes>
       </main>
       <Footer />
-      <script type="text/javascript" src="./Assets/aurora/scripts/color-scheme-toggle.js"></script>
     </div>
   );
 }
