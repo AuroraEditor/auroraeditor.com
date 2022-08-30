0b8d1c464d7984672ebc359b7e1e52e6d0b833ab
Wesley De Groot <email@wesleydegroot.nl>
2022-08-30 17:04:11 +0200

    Update ThemeSelection.jsx

--- src/Base/Components/ThemeSelection.jsx
+++ src/Base/Components/ThemeSelection.jsx
@@ -6,15 +6,15 @@ function ThemeSelection() {
         <div className="content">
             <div className="color-scheme-toggle" role="radiogroup" tabIndex={0} aria-label="Select a color scheme preference">
                 <label data-color-scheme-option="light">
-                    <input type="radio" name="colorToggle" value="light" onClick="setColorScheme('light')" autoComplete="off" />
+                    <input type="radio" name="colorToggle" value="light" autoComplete="off" />
                     <div className="text">Light</div>
                 </label>
                 <label data-color-scheme-option="dark">
-                    <input type="radio" name="colorToggle" value="dark" onClick="setColorScheme('dark')" autoComplete="off" />
+                    <input type="radio" name="colorToggle" value="dark" autoComplete="off" />
                     <div className="text">Dark</div>
                 </label>
                 <label data-color-scheme-option="auto">
-                    <input type="radio" name="colorToggle" value="auto" onClick="setColorScheme('auto')" autoComplete="off" />
+                    <input type="radio" name="colorToggle" value="auto" autoComplete="off" />
                     <div className="text">Auto</div>
                 </label>
             </div>
