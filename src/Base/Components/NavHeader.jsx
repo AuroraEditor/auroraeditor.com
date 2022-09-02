23d6419ff75e1e65967113a0d2dbeaec7038f009
Wesley De Groot <email@wesleydegroot.nl>
2022-09-02 10:06:01 +0200

    Update NavHeader.jsx

--- src/Base/Components/NavHeader.jsx
+++ src/Base/Components/NavHeader.jsx
@@ -35,9 +35,9 @@ function NavHeader() {
                                     <span className="auroranav-menucta-chevron"></span>
                                 </label>
                             </div>
-                            <div className="auroranav-action auroranav-action-button" aria-disabled>
+                            <div className="auroranav-action auroranav-action-button">
                                 <div className="button-multi-container">
-                                    <a href="/#" className="auroranav-button button button-compact button-pill button-multi" id="button-multi-1">Coming Soon</a>
+                                    <a href="https://github.com/AuroraEditor/AuroraEditor" className="auroranav-button button button-compact button-pill button-multi" id="button-multi-1">GitHub repo</a>
                                 </div>
                             </div>
                         </div>
