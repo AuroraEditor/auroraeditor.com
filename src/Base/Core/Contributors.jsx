64c9109511cc2cdb7ecd56967e3d498fafa2184e
Wesley de Groot <email@wesleydegroot.nl>
2022-10-08 19:17:22 +0200

    Create contributors page

---
+++ src/Base/Core/Contributors.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Assets/aurora/styles/aurora-style.css";
+
+function Contributors() {
+    return (
+        <aside class="section contributors-generator">
+            <center>LOADING CONTRIBUTORS,<br />Please wait...</center>
+        </aside>
+    )
+}
+
+export default Contributors;
