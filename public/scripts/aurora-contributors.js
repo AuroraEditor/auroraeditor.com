3a51d6a97ce93b0b77e689978f16e465eb7e9fb4
Nanashi Li <tihan-nico@plexus.dev>
2022-10-08 17:24:26 +0200

    Update contributes section

--- public/scripts/aurora-contributors.js
+++ public/scripts/aurora-contributors.js
@@ -8,6 +8,12 @@ function httpGet(theUrl, callback) {
     xmlHttp.send(null)
 }
 
+function makeUpperCaseAfterCommas(str) {
+    return str.replace(/,\s*([a-z])/g, function (d, e) {
+        return ", " + e.toUpperCase()
+    });
+}
+
 addEventListener('load', function () {
     console.log('Getting latest contributors');
     httpGet("https://raw.githubusercontent.com/AuroraEditor/AuroraEditor/main/.all-contributorsrc", function (contents) {
@@ -15,15 +21,14 @@ addEventListener('load', function () {
         var data = JSON.parse(contents);
         var max = data.contributorsPerLine || 4;
         var count = 0;
-        var generated = '<aside class="section contributors"><div class="section-content">';
+        var generated = '<aside class="section contributers-links contributors"><div class="section-content">';
         var endRow = '</div>';
         var beginRow = '<div class="row">';
         var person = `
-        <div class="column large-3 medium-6 small-12">
+        <div class="section-content column large-3 medium-6 small-12">
             <a href="PROFILE" class="block text-center">
-                <img class="rounded" src="AVATAR" width="100" height="100">
+                <img class="contributer-image" src="AVATAR" width="100" height="100">
                 <p><strong>NAME</strong></p>
-                <p>LOGIN</p>
                 <p class="typography-subbody">BIO</p>
             </a>
         </div>`;
@@ -36,12 +41,13 @@ addEventListener('load', function () {
             //     count = 0;
             // }
 
+            const bio = makeUpperCaseAfterCommas(contributor.contributions.join(", "))
+
             generated += person
                 .replace('PROFILE', contributor.profile)
                 .replace('AVATAR', contributor.avatar_url)
-                .replace('LOGIN', contributor.login)
                 .replace('NAME', contributor.name)
-                .replace('BIO', contributor.contributions.join(', '));
+                .replace('BIO', bio);
 
             count += 1;
         });
