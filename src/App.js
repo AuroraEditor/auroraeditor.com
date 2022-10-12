1c772523d1ef570983273463e15911364ba068c5
Nanashi Li <tihan-nico@plexus.dev>
2022-10-12 23:28:58 +0200

    Added account verification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./Assets/aurora/styles/aurora-global.css"
 import useScript from './Base/Hooks/useScript';
-
 import ProfileNavHeader from './Base/Components/Profile/ProfileNavHeader';
 import AuthNavHeader from './Base/Components/Auth/AuthNavHeader';
 import NavHeader from "./Base/Components/NavHeader";
-
 import MainPage from "./Base/Core/Main";
 import Login from "./Base/Core/Auth/Login";
 import Register from './Base/Core/Auth/Register';
@@ -14,20 +12,15 @@ import Contributors from "./Base/Core/Contributors";
 import Included from './Base/Core/Included';
 import Terms from './Base/Core/Terms';
 import PageNotFound from "./Base/Core/PageNotFound";
-
 import Ribbon from './Base/Components/Ribbon';
 import Footer from "./Base/Components/Footer";
-
-const routes = [
-  { path: "/", element: <MainPage /> },
-  { path: "/sign-in", element: <Login /> },
-  { path: "/sign-up", element: <Register /> },
-  { path: "/account", element: <Profile /> },
-  { path: "/contributors", element: <Contributors /> },
-  { path: "/included", element: <Included /> },
-  { path: "/legal/terms", element: <Terms /> },
-  { path: "/pnf", element: <PageNotFound /> }
-];
+import Terms from './Base/Core/Terms';
+import Login from "./Base/Core/Auth/Login";
+import Register from './Base/Core/Auth/Register';
+import AuthNavHeader from './Base/Components/Auth/AuthNavHeader';
+import Profile from './Base/Core/Profile/Profile';
+import ProfileNavHeader from './Base/Components/Profile/ProfileNavHeader';
+import EmailVerification from "./Base/Core/Auth/EmailVerification"
 
 function App() {
   useScript("/scripts/aurora-nav.js")
@@ -36,17 +29,34 @@ function App() {
 
   const { pathname } = useLocation();
 
+  var routes = [
+    { path: "/", element: <MainPage /> },
+    { path: "/sign-in", element: <Login /> },
+    { path: "/oauth/:email-verification", element: <EmailVerification/> },
+    { path: "/sign-up", element: <Register /> },
+    { path: "/account", element: <Profile /> },
+    { path: "/contributors", element: <Contributors /> },
+    { path: "/included", element: <Included /> },
+    { path: "/legal/terms", element: <Terms /> },
+    { path: "/pnf", element: <PageNotFound /> }
+  ]
+
   return (
     <div id="overview" className="dmf" data-color-scheme="light" style={{ scrollBehavior: 'smooth' }}>
 
-      {pathname !== "/account" && pathname !== "/sign-in" && pathname !== "/sign-up" && <Ribbon />}
+      {pathname !== "/account" 
+      && pathname !== "/sign-in" 
+      && pathname !== "/sign-up"
+      && pathname !== "/oauth/email-verification" && <Ribbon />}
 
       <input type="checkbox" id="auroranav-menustate" className="auroranav-menustate" />
       <div id="auroranav-sticky-placeholder" className="css-sticky auroranav-sticking" />
 
       {
-          pathname === "/account" ? <ProfileNavHeader /> :
-          pathname === "/sign-in" || pathname === "/sign-up" ? <AuthNavHeader /> : <NavHeader />
+        pathname === "/account" ? <ProfileNavHeader /> :
+          pathname === "/sign-in" 
+          || pathname === "/sign-up"
+          || pathname === "/oauth/email-verification"  ? <AuthNavHeader /> : <NavHeader />
       }
 
       <label id="auroranav-curtain" htmlFor="auroranav-menustate" />
@@ -60,7 +70,10 @@ function App() {
         </Routes>
       </main>
 
-      {pathname !== "/sign-in" && pathname !== "/sign-up" && <Footer />}
+      {pathname !== "/sign-in" 
+      && pathname !== "/sign-up" 
+      && pathname !== "/oauth/email-verification"
+      && <Footer />}
     </div>
   );
 }
