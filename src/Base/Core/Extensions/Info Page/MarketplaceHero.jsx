51f90cb2c40f1420ba5c5ebd959d0479a5677a25
Nanashi Li <nanashili@plexus.dev>
2022-10-21 13:35:45 +0200

    Fix hero intro padding

--- src/Base/Core/Extensions/Info Page/MarketplaceHero.jsx
+++ src/Base/Core/Extensions/Info Page/MarketplaceHero.jsx
@@ -13,16 +13,14 @@ function MarketplaceHero() {
                     </picture>
                     <h1 className="hero-eyebrow typography-eyebrow-super">Marketplace</h1>
                     <h2 className="hero-headline typography-headline-super">
-                        Extensions everyone loves.
-                        <br />
-                        From a place you can trust.
+                        A curated space for extensions.
                     </h2>
                 </div>
             </div>
-            <div className="here-intro">
+            <div className="hero-intro">
                 <div className="section-content">
                     <p className="hero-copy typography-callout">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        The Extensions Marketplace is a curated space where developers from all over can discover extensions to improve their developer experience and upload their own extension to share with the world.
                     </p>
                 </div>
             </div>
