aad216799ab007a602eaa25ba4a32ba1b664054a
Wesley de Groot <email@wesleydegroot.nl>
2022-11-24 22:05:14 +0100

    Fix lint error

--- src/Base/Components/NavHeader.jsx
+++ src/Base/Components/NavHeader.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import '../../Assets/aurora/styles/aurora-nav.css';
-import { getOS } from "../Utils/DeviceDetection";
 
 function NavHeader() {
     return (
