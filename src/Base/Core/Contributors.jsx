5124446ee9b304915163b31541f0bf2fc7a1948c
Wesley de Groot <email@wesleydegroot.nl>
2022-12-06 16:24:41 +0100

    Update Contributors.jsx

--- src/Base/Core/Contributors.jsx
+++ src/Base/Core/Contributors.jsx
@@ -35,7 +35,7 @@ function ContributorPlaceholder() {
     return (
       <div class="section-content column large-3 medium-6 small-12">
         <span class="block text-center">
-          <img class="contributor-image shimmer" alt="Loading contributors" src="https://avatars.githubusercontent.com/u/106490518?s=128&v=4" width="100" height="100" />
+          <span class="contributor-image shimmer" alt="Loading contributors" src="https://avatars.githubusercontent.com/u/106490518?s=128&v=4" width="100" height="100">&nbsp;</span>
           <p><strong class="shimmer">LOADING NAME</strong></p>
           <p class="typography-subbody shimmer">FUNC1, FUNC2, FUNC3, FUNC4</p>
         </span>
