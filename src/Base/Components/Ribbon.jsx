5addb63dfd9331e98b0829eec2d8df6b24276ec7
Wesley de Groot <email@wesleydegroot.nl>
2023-04-03 10:42:30 +0200

    Update Ribbon.jsx

--- src/Base/Components/Ribbon.jsx
+++ src/Base/Components/Ribbon.jsx
@@ -6,7 +6,7 @@ function Ribbon() {
             <div className="ribbon-content-wrapper">
                 <div className="ribbon-content row">
                     <div className="column large-12 large-centered">
-                        <p>Currently in development, <a href="https://github.com/orgs/AuroraEditor/projects/1/views/2" id="suggest-link" className="ribbon-link more">check out the roadmap</a>.</p>
+                        <p>Currently not in active development, <a href="https://github.com/orgs/AuroraEditor/projects/1/views/2" id="suggest-link" className="ribbon-link more">check out the roadmap</a>.</p>
                     </div>
                 </div>
             </div>
